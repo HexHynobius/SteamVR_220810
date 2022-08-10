ff90c10c6874b084c8597f21507ea273b39416e4
HexHynobius <105775270+HexHynobius@users.noreply.github.com>
2022-08-10 16:34:44 +0800

    Updata 0810 跟新UI

---
+++ Assets/Re.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Re : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(Mathf.Clamp(gameObject.transform.position.x,-2.5f,3.5f), gameObject.transform.position.y, Mathf.Clamp(gameObject.transform.position.z, 0,4.5f));
+    }
+}
